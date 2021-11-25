c57cc9a6d8793dd6517a879ecdb000b8cf99af72
Shivam Patel <tsc7795@gmail.com>
2021-11-25 15:01:40 +0530

    Added post model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ssi-blog-project
 This Blog is About foreign university scholarships  programmes 
+
+Tasks: 28/10/2021
+
+-update your project and push it to github
+-why we use, at the end in tuple. eg("tuple",)
+-what is the difference between auto_now_add and auto_now
+-what is the use of makemigrations and migrate
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):  # class name is given whatever table name we want
+    STATUS_CHOICEs = (           
+        ("draft", "Draft"),
+        ("published", "Published",)
+    )
+    title = models.CharField(
+        max_length=225
+    )  # whatever the name you are giving it will creat a coloumn in table
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+        )
+    content = models.TextField()
+    published_at = models.DateTimeField(default = timezone.now)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICEs)
+
+    class Meta:
+        ordering = ("-published_at",)  #tuple
+
+    def __str__(self):
+        #return self.title
+        return f"{str(self.id)}-{self.title}"
+        #return str(self.id)+self.title
+
+
